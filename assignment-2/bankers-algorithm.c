3f027e75c937c8dda1846a72ac9983a6ce0c0865
FoundIzalith <b17kaylaeva@gmail.com>
2022-11-20 18:59:44 -0500

    Add safety algorithm

--- assignment-2/bankers-algorithm.c
+++ assignment-2/bankers-algorithm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define n_Resources 3
 #define n_Processes 5
@@ -10,13 +11,16 @@ int max[n_Processes][n_Resources]; //Max of a given resource a process may reque
 int allocation[n_Processes][n_Resources]; //Current # of resources allocated to process
 int need[n_Processes][n_Resources]; //# of resources a process needs
 
-int readInFile(char **argv);
-
+int readInFile(char **argv); 
+bool safety();
+void allocate();
+ 
 //Pass data file as input
 int main(int argc, char **argv) {
     if (argv[1] == NULL) {
-        printf("bankers-algorithm requires a minimum of 1 parameter.");
-        printf("Please run as: ./bankers-algorithm [filename]");
+        printf("bankers-algorithm requires a minimum of 1 parameter.\n");
+        printf("Please run as: ./bankers-algorithm [filename]\n");
+        return 1;
     }
 
     //File input functions moved to another function for the sake of readability 
@@ -24,9 +28,21 @@ int main(int argc, char **argv) {
     int read = readInFile(argv);
     if (read == 1) return 1; 
 
+    //Set need and finished initial values 
+    for(int i = 0; i < n_Processes; i++) {
+        for(int j = 0; j < n_Resources; j++) {
+            need[i][j] = max[i][j] - allocation[i][j];
+        }
+    }
+
+    bool safe = safety();
+    
+
     return 0;
 }
 
+//Returns 0 if read is successful, else returns 1
+//Side effect: sets values of all the banker's data
 int readInFile(char **argv) {
     FILE *file = fopen(argv[1], "r");
 
@@ -37,7 +53,7 @@ int readInFile(char **argv) {
 
     printf("Successfully opened \"%s\"\n", argv[1]);
 
-    char buffer[100];
+    char buffer[32];
 
     //I hate to code a file input like this but I think a better solution
     //would be overly complicated for this sort of project. I know this is awful.
@@ -61,4 +77,39 @@ int readInFile(char **argv) {
     fclose(file);
 
     return 0;
+}
+
+bool safety() {
+    bool safe; 
+    bool finished[n_Processes];
+    int work[n_Resources];
+
+    //Set initial values of finished and work arrays
+    for(int i = 0; i < n_Processes; i++) {
+        finished[i] = false;
+    }
+
+    for(int j = 0; j < n_Resources; j++) {
+        work[j] = *allocation[j];
+    }
+
+    //Find process (i) such that both finished[i] == false and need[i] <= work 
+    for(int i = 0; i < n_Processes; i++) {
+        for(int j = 0; j < n_Resources; j++) {
+            if(finished[i] == false && need[i][j] <= work[j]) { 
+                work[j] = work[j] + allocation[i][j];
+                finished[i] = true;
+            }
+        }
+    }
+
+    //If all processes are finished then the system is in a safe state 
+    bool allSafe = false;
+    int counter = 0; 
+    for(int i = 0; i < n_Processes; i++) {
+        if (finished[i] == true) ++counter;
+        if (counter == n_Processes) allSafe = true;
+    }
+
+    return allSafe;
 }
